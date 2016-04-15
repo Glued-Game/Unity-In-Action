88d4e8212706098152cc52a9ad8457b80def3011
shohan4556 <shohan4556@gmail.com>
2016-04-16 01:18:04 +0600

    Mouse look

---
+++ chapter-02-FPS/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour {
+
+	public enum RotationAxes{
+		MouseXandY = 0,
+		MouseX = 1,
+		MouseY = 2
+	}
+	public float speedXY = 5f; // mouse movement speed horizontal and vertical
+	public RotationAxes axes = RotationAxes.MouseXandY;
+	private float minVerticalY = -45f;
+	private float maxVerticalY = 45f;
+	private float _rotationX = 0; // declared for vertical angle
+
+	// Update is called once per frame
+	void Update () {
+		if(axes == RotationAxes.MouseX){
+			// horizontal rotation here
+			transform.Rotate (0, Input.GetAxis("Mouse X")*speedXY, 0f);
+			print ("move Horizontally");
+		}
+		else if(axes == RotationAxes.MouseY){
+			// vertical rotation here
+			_rotationX -= Input.GetAxis ("Mouse Y") * speedXY;
+			_rotationX = Mathf.Clamp (_rotationX, minVerticalY, maxVerticalY);
+
+			float _rotationY = transform.localEulerAngles.y;
+			transform.localEulerAngles = new Vector3 (_rotationX, _rotationY, 0);
+		}
+		else{
+			// both rhorizontal and vertical rotaion here
+		}
+	}
+}
---
+++ chapter-02-FPS/Assets/Scripts/TestScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+
+	private float speed = 1.5f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0, speed, 0, Space.World);
+	}
+}
